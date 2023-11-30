3f5a5e88bcd6cd298faa20793b05b8ea07480c6d
Max Kuhn <mxkuhn@gmail.com>
2023-11-29 21:53:50 -0500

    initial draft of chapter

--- R/setup_hotel_rates.R
+++ R/setup_hotel_rates.R
@@ -1,8 +1,7 @@
-
-hotel_rate_split <- initial_validation_time_split(hotel_rates, prop = c(0.7, 0.15))
+hotel_rates <- hotel_rates %>% arrange(arrival_date)
+hotel_rate_split <- initial_time_split(hotel_rates, prop = c(0.75))
 hotel_rate_train <- training(hotel_rate_split)
-hotel_rate_val   <- testing(hotel_rate_split)
-hotel_rate_test  <- validation(hotel_rate_split)
+hotel_rate_test  <- testing(hotel_rate_split)
 
 set.seed(6858)
-hotel_rate_rs  <- validation_set(hotel_rate_split, strata = avg_price_per_room)
+hotel_rate_rs  <- vfold_cv(hotel_rate_train, strata = avg_price_per_room)
