6a4a02bc6ed0a2bb40e3deed1f6b920a47dea3c1
topepo <mxkuhn@gmail.com>
2023-12-02 22:32:22 -0500

    initial add/remove chapter

--- R/setup_hotel_rates.R
+++ R/setup_hotel_rates.R
@@ -1,28 +1,7 @@
-
-# ------------------------------------------------------------------------------
-# Initially split the data and then use the date range to make the real split so
-# that there are no overlapping days.
-
-hotel_rate_split <- initial_time_split(hotel_rates, prop = 0.9)
+hotel_rates <- hotel_rates %>% arrange(arrival_date)
+hotel_rate_split <- initial_time_split(hotel_rates, prop = c(0.75))
 hotel_rate_train <- training(hotel_rate_split)
-hotel_rate_train_range <- range(hotel_rate_train$arrival_date)
-
-hotel_rate_train <-
-  hotel_rates %>%
-  filter(arrival_date <= hotel_rate_train_range[2]) %>%
-  select(-arrival_date)
-
-hotel_rate_test <-
-  hotel_rates %>%
-  filter(arrival_date > hotel_rate_train_range[2])
-
-hotel_rate_test_range <- range(hotel_rate_test$arrival_date)
-hotel_rate_train_range <- format(hotel_rate_train_range, "%e %B %Y")
-hotel_rate_test_range <- format(hotel_rate_test_range, "%e %B %Y")
-
-hotel_rate_test <-
-  hotel_rate_test %>%
-  select(-arrival_date)
+hotel_rate_test  <- testing(hotel_rate_split)
 
 set.seed(6858)
-hotel_rate_rs  <- validation_split(hotel_rate_train, strata = avg_price_per_room)
+hotel_rate_rs  <- vfold_cv(hotel_rate_train, strata = avg_price_per_room)
