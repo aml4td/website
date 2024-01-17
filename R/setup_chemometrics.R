f86a78254ea427005e56890f50297b15cf6c6cb3
‘topepo’ <mxkuhn@gmail.com>
2024-01-17 09:30:59 -0500

    MDS draft

--- R/setup_chemometrics.R
+++ R/setup_chemometrics.R
@@ -4,14 +4,24 @@ chimiometrie_2019 <-
   data_chimiometrie_2019()  %>%
   select(-soy_oil, -lucerne)
 
-set.seed(87)
-barley_split <-
-  initial_split(chimiometrie_2019,
-                prop = 1 - (500 / nrow(chimiometrie_2019)))
-barley_not_test <- training(barley_split)
-barley_test  <-  testing(barley_split)
+barley_breaks <- (0:27) * 2
 
-set.seed(2323)
-barley_rs <- validation_split(barley_not_test, prop = 1 - (500 / nrow(barley_not_test)))
-barley_train <- analysis(barley_rs$splits[[1]])
-barley_val <- assessment(barley_rs$splits[[1]])
+chimiometrie_2019$barley_bin <-
+  cut(chimiometrie_2019$barley,
+      breaks = barley_breaks,
+      include.lowest = TRUE) 
+
+set.seed(101)
+barley_split <- initial_validation_split(chimiometrie_2019, prop = c(0.7, 0.15), strata = barley)
+barley_train <- training(barley_split)
+barley_val <- validation(barley_split)
+barley_test <- testing(barley_split)
+barley_rs <- validation_set(barley_split)
+
+wave <- tibble(index = 1:550, wavelength = seq(1300, 2398, by = 2))
+wave_corr <- 
+  barley_train %>% 
+  select(starts_with("wv")) %>% 
+  cor()
+wave_corr <- wave_corr[upper.tri(wave_corr)]
+
