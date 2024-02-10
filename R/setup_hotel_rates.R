926747e2029f929d383cee211db09ecc522ec9a9
‘topepo’ <mxkuhn@gmail.com>
2024-02-09 22:22:14 -0600

    undue bad conflict resolution

--- R/setup_hotel_rates.R
+++ R/setup_hotel_rates.R
@@ -3,5 +3,20 @@ hotel_rate_split <- initial_time_split(hotel_rates, prop = c(0.75))
 hotel_rate_train <- training(hotel_rate_split)
 hotel_rate_test  <- testing(hotel_rate_split)
 
-set.seed(6858)
-hotel_rate_rs  <- vfold_cv(hotel_rate_train, strata = avg_price_per_room)
+hotel_rate_rs  <-
+  sliding_period(
+    hotel_rate_train,
+    index = arrival_date,
+    period = "week",
+    lookback = Inf,
+    complete = TRUE,
+    assess_stop = 2,
+    step = 2,
+    skip = 15
+  )
+
+# To be more informative, we'll alter the ID column to be the first day of 
+# the holdout set
+holdout_date <- 
+  map_chr(hotel_rate_rs$splits, ~ as.character(min(assessment(.x)$arrival_date)))
+hotel_rate_rs$id <- holdout_date
