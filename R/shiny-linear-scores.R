af14d997d939769c0d5d14edc80036b050b57dc4
‘topepo’ <mxkuhn@gmail.com>
2024-05-09 10:40:28 -0400

    re-organize shiny files

--- R/shiny-linear-scores.R
+++ R/shiny-linear-scores.R
@@ -1,37 +1,4 @@
-library(shiny)
-library(ggplot2)
-library(bslib)
-library(viridis)
-library(dplyr)
-library(tidyr)
-library(purrr)
-library(ggforce)
 
-# ------------------------------------------------------------------------------
-
-light_bg <- "#fcfefe" # from aml4td.scss
-grid_theme <- bs_theme(
-  bg = light_bg, fg = "#595959"
-)
-
-# ------------------------------------------------------------------------------
-
-theme_light_bl<- function(...) {
-  
-  ret <- ggplot2::theme_bw(...)
-  
-  col_rect <- ggplot2::element_rect(fill = light_bg, colour = light_bg)
-  ret$panel.background  <- col_rect
-  ret$plot.background   <- col_rect
-  ret$legend.background <- col_rect
-  ret$legend.key        <- col_rect
-  
-  ret$legend.position <- "top"
-  
-  ret
-}
-
-# ------------------------------------------------------------------------------
 
 ui <- page_fillable(
   theme = bs_theme(bg = "#fcfefe", fg = "#595959"),
