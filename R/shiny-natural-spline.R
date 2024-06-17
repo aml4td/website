1c1254c1d5a3f0eab8b7998a59ccbb6700896db5
‘topepo’ <mxkuhn@gmail.com>
2024-06-16 21:24:23 -0400

    remove old theme

--- R/shiny-natural-spline.R
+++ R/shiny-natural-spline.R
@@ -25,31 +25,7 @@ server <- function(input, output, session) {
   theme_set(theme_transparent())
   
   # ------------------------------------------------------------------------------
-  
-  theme_light_bl<- function(...) {
-    
-    ret <- ggplot2::theme_bw(...)
-    
-    col_rect <- ggplot2::element_rect(fill = light_bg, colour = light_bg)
-    ret$panel.background  <- col_rect
-    ret$plot.background   <- col_rect
-    ret$legend.background <- col_rect
-    ret$legend.key        <- col_rect
-    
-    larger_x_text <- ggplot2::element_text(size = rel(1.25))
-    larger_y_text <- ggplot2::element_text(size = rel(1.25), angle = 90)
-    ret$axis.text.x <- larger_x_text
-    ret$axis.text.y <- larger_y_text
-    ret$axis.title.x <- larger_x_text
-    ret$axis.title.y <- larger_y_text  
-    
-    ret$legend.position <- "top"
-    
-    ret
-  }
-  
-  col_rect <- ggplot2::element_rect(fill = light_bg, colour = light_bg)
-  
+
   maybe_lm <- function(x) {
     try(lm(y ~ poly(x, input$piecewise_deg), data = x), silent = TRUE)
   }
@@ -98,8 +74,7 @@ server <- function(input, output, session) {
     spline_example %>%
     ggplot(aes(x = x, y = y)) +
     geom_point(alpha = 3 / 4, pch = 1, cex = 3) +
-    labs(x = "Age", y = "Isotope Ratio") +
-    theme_light_bl()
+    labs(x = "Age", y = "Isotope Ratio") 
   
   output$spline <- renderPlot({
     
