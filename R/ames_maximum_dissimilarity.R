933d52c20942844f28c958e3e304359996223ab6
‘topepo’ <mxkuhn@gmail.com>
2024-07-02 20:27:08 -0400

    ames gif in dark adn light

--- R/ames_maximum_dissimilarity.R
+++ R/ames_maximum_dissimilarity.R
@@ -2,6 +2,7 @@ library(caret)
 library(tidymodels)
 library(sf)
 library(gganimate)
+library(ggdark)
 
 # ------------------------------------------------------------------------------
 
@@ -11,6 +12,21 @@ options(pillar.advice = FALSE, pillar.min_title_chars = Inf)
 ia_roads <- st_read(dsn = "R/iowa_highway.shp") # also requires iowa_highway.SHX
 
 light_bg <- "#fcfefe"
+
+theme_transparent <- function(...) {
+  
+  ret <- ggplot2::theme_bw(...)
+  
+  transparent_rect <- ggplot2::element_rect(fill = "transparent", colour = NA)
+  ret$panel.background  <- transparent_rect
+  ret$plot.background   <- transparent_rect
+  ret$legend.background <- transparent_rect
+  ret$legend.key        <- transparent_rect
+  
+  ret$legend.position <- "top"
+  
+  ret
+}
 
 # ------------------------------------------------------------------------------
 
@@ -50,31 +66,16 @@ ames_seq <-
   bind_rows(ames_start, ames_pool[selection_path,]) %>%
   mutate(iteration = row_number())
 
-# ------------------------------------------------------------------------------
-
 ames_x <- extendrange(ames$Longitude)
 ames_y <- extendrange(ames$Latitude)
+
+# ------------------------------------------------------------------------------
+
 
 all_ames <-
   ggplot() +
   xlim(ames_x) +
   ylim(ames_y) +
-  theme(
-    panel.background = element_rect(fill = light_bg, colour = NA),
-    plot.background = element_rect(fill = light_bg, colour = NA),
-    panel.grid = element_blank(),
-    panel.border = element_blank(),
-    plot.margin = unit(c(0, 0, 0, 0), "null"),
-    panel.margin = unit(c(0, 0, 0, 0), "null"),
-    axis.ticks = element_blank(),
-    axis.text = element_blank(),
-    axis.title = element_blank(),
-    axis.line = element_blank(),
-    legend.position = "none",
-    axis.ticks.length = unit(0, "null"),
-    axis.ticks.margin = unit(0, "null"),
-    legend.margin = margin(0, 0, 0, 0, "null")
-  ) +
   geom_sf(data = ia_roads, aes(geometry = geometry), alpha = .1) +
   geom_point(
     data = ames, aes(x = Longitude, y = Latitude),
@@ -83,9 +84,6 @@ all_ames <-
 
 # ------------------------------------------------------------------------------
 
-base_cols <- RColorBrewer::brewer.pal(9, "YlOrRd")[-(1:2)]
-ames_pal <- colorRampPalette(RColorBrewer::brewer.pal(9, "YlOrRd")[-(1:2)])(n_selected)
-
 if (interactive()) {
   all_ames +
     geom_point(
@@ -93,12 +91,34 @@ if (interactive()) {
       aes(x = Longitude, y = Latitude, col = format(iteration), size = -iteration),
       show.legend = FALSE
     ) +
-    scale_color_manual(values = ames_pal) +
+    scale_color_manual(values = ames_light_pal) +
     scale_size(range = c(3, 8))
 }
 
-md_anim <-
+# ------------------------------------------------------------------------------
+
+light_cols <- RColorBrewer::brewer.pal(9, "YlOrRd")[-(1:2)]
+ames_light_pal <- colorRampPalette(RColorBrewer::brewer.pal(9, "YlOrRd")[-(1:2)])(n_selected)
+
+md_light_anim <-
   all_ames +
+  theme_transparent() +
+  theme(
+    panel.grid = element_blank(),
+    panel.border = element_blank(),
+    plot.margin = unit(c(0, 0, 0, 0), "null"),
+    panel.margin = unit(c(0, 0, 0, 0), "null"),
+    axis.ticks = element_blank(),
+    axis.text = element_blank(),
+    axis.title = element_blank(),
+    axis.line = element_blank(),
+    panel.background = element_rect(fill = light_bg, colour = NA),
+    plot.background = element_rect(fill = light_bg, colour = NA),
+    legend.position = "none",
+    axis.ticks.length = unit(0, "null"),
+    axis.ticks.margin = unit(0, "null"),
+    legend.margin = margin(0, 0, 0, 0, "null")
+  ) +
   geom_point(
     data = ames_seq,
     aes(x = Longitude, y = Latitude,col = format(iteration), size = -iteration),
@@ -111,11 +131,11 @@ md_anim <-
   ) +
   shadow_mark() +
   labs(title = "{closest_state} points selected") +
-  scale_color_manual(values = ames_pal) +
+  scale_color_manual(values = ames_light_pal) +
   scale_size(range = c(3, 8))
 
 animate(
-  md_anim,
+  md_light_anim,
   device = "png",
   detail = 5,
   width = 1500,
@@ -125,5 +145,56 @@ animate(
   duration = 25
 )
 
-anim_save("premade/anime_ames_selection.gif")
+anim_save("premade/anime-ames-selection-light.gif")
 
+# ------------------------------------------------------------------------------
+
+ggplot2::reset_theme_settings()
+ggplot2::theme_set(ggdark::dark_theme_grey())
+
+md_dark_anim <-
+  all_ames +
+  theme_transparent() +
+  theme(
+    panel.grid = element_blank(),
+    panel.border = element_blank(),
+    plot.margin = unit(c(0, 0, 0, 0), "null"),
+    panel.margin = unit(c(0, 0, 0, 0), "null"),
+    axis.ticks = element_blank(),
+    axis.text = element_blank(),
+    axis.title = element_blank(),
+    axis.line = element_blank(),
+    panel.background = element_rect(fill = "#000000", colour = NA),
+    plot.background = element_rect(fill = "#000000", colour = NA),
+    legend.position = "none",
+    axis.ticks.length = unit(0, "null"),
+    axis.ticks.margin = unit(0, "null"),
+    legend.margin = margin(0, 0, 0, 0, "null")
+  ) +
+  geom_point(
+    data = ames_seq,
+    aes(x = Longitude, y = Latitude,col = format(iteration), size = -iteration),
+    show.legend = FALSE
+  ) +
+  transition_states(
+    iteration,
+    transition_length = 2,
+    state_length = 1
+  ) +
+  shadow_mark() +
+  labs(title = "{closest_state} points selected") +
+  scale_color_manual(values = ames_light_pal) +
+  scale_size(range = c(3, 8))
+
+animate(
+  md_dark_anim,
+  device = "png",
+  detail = 5,
+  width = 1500,
+  height = 1000,
+  res = 200,
+  renderer = gifski_renderer(),
+  duration = 25
+)
+
+anim_save("premade/anime-ames-selection-dark.gif")
