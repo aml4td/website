36ba5268d6806657582325f1fcdf07f175e5a9d5
Max Kuhn <mxkuhn@gmail.com>
2024-08-31 16:50:55 -0400

    update some code and convert format to bslib (#50)

--- R/shiny-imputation-exploration.R
+++ R/shiny-imputation-exploration.R
@@ -1,91 +1,99 @@
-#move the library calls to the shiny live code chunk
-library(tidyverse)
-library(recipes)
+ui <- fluidPage(
+  theme = grid_theme,
+  fluidRow(
+    column(
+      width = 2,
+      radioButtons(
+        "selection", "Select data:",
+        c(`Linear` = "LINEAR", `Nonlinear` = "NONLINEAR")
+      )
+    ), # data type
+    column(
+      width = 5,
+      sliderInput("pctMissing", "Percent Missing:", min = 1, max = 30, value = 10, step = 1)
+    ), # amount missing
+    
+    column(
+      width = 5,
+      selectInput(
+        "imputeType", "Imputation Technique:",
+        c(`Mean` = "MEANIMPUTE",
+          `Median` = "MEDIANIMPUTE",
+          `Bagged Tree` = "BAGIMPUTE",
+          `k-NN` = "KNNIMPUTE",
+          `Linear Regression` = "LINEARIMPUTE"
+        )
+      )
+    ),  # imputation method
+    fluidRow(
+      column(
+        width = 2,
+        actionButton("update", "Update")
+      ),
+      column(
+        width = 8,
+        align = "center",
+        plotOutput('plot')
+      )
+    )
+  ) # top fluid row
+)
 
 load(url("https://raw.githubusercontent.com/aml4td/website/main/RData/simulation_data.RData"))
 
-ui <-
-  fluidPage(
-    titlePanel("Imputation Exploration"),
-    
-    sidebarLayout(
-      sidebarPanel(
-        selectInput(
-          "selection", "Select data:",
-          c(`Linear` = "LINEAR",
-            `Nonlinear` = "NONLINEAR"
-            )
-          ),
-        hr(),
-        sliderInput("pctMissing", "Percent Missing:", min=1, max=90, value=10, step=1),
-        hr(),
-        selectInput(
-          "imputeType", "Imputation Technique:",
-          c(`Mean Imputation` = "MEANIMPUTE",
-            `Median Imputation` = "MEDIANIMPUTE",
-            `Bag Imputation` = "BAGIMPUTE",
-            `k-NN Imputation` = "KNNIMPUTE",
-            `Linear Imputation` = "LINEARIMPUTE"
-            )
-          ),
-        actionButton("update", "Update"),
-        hr()
-      ),
-      
-      mainPanel(
-        plotOutput("plot")
-      )
-    )
-  )
-
 server <-
   function(input, output, session) {
-    terms <- reactive({
+    update_app <- reactive({
       input$update
       isolate({
         withProgress({
           setProgress(message = "Updating...")
-          getData(DATA=input$selection, 
-                  PCTMISSING=input$pctMissing, 
-                  IMPUTETYPE=input$imputeType)
+          getData(DATA = input$selection,
+                  PCTMISSING = input$pctMissing,
+                  IMPUTETYPE = input$imputeType)
         })
       })
     })
     
     output$plot <- renderPlot({
-      v <- terms()
+      v <- update_app()
       
       f1 <-
-        ggplot(v$for_figure, aes(x=x1, y=x2, pch=Obs_Type, color=Obs_Type)) +
-        facet_grid(cols=vars(Type)) +
+        ggplot(v$for_figure, aes(x1, x2, pch = Obs_Type, color = Obs_Type)) +
+        facet_grid(cols = vars(Type)) +
         geom_point(size=3) +
-        scale_color_manual(values=c("#56B4E9", "#D55E00", "#56B4E9", "#D55E00")) +
-        scale_shape_manual(values=c(16,17,1,2)) +
-        xlim(v$xlims) +
-        ylim(v$ylims) +
-        xlab("Predictor 1") +
-        ylab("Predictor 2") +
-        theme_bw() +
-        theme(axis.title.x = element_text(size=24),
-              axis.text.x=element_text(size=12),
-              axis.title.y = element_text(size=24),
-              axis.text.y = element_text(size=12),
-              strip.text = element_text(size = 16),
-              legend.position = "none")
+        scale_color_manual(values = c("#56B4E9", "#D55E00", "#56B4E9", "#D55E00")) +
+        scale_shape_manual(values = c(16, 17, 1, 2)) +
+        lims(x = v$xlims, y = v$ylims) +
+        labs(x = "Predictor 1", y = "Predictor 2") +
+        theme_light_bl() +
+        theme(
+          axis.title.x = element_text(size = 24),
+          axis.text.x = element_text(size = 12),
+          axis.title.y = element_text(size = 24),
+          axis.text.y = element_text(size = 12),
+          strip.text = element_text(size = 16),
+          legend.position = "none"
+        )
       
       if(v$linear_partition){
-        f1 + geom_abline(slope=1, intercept=4, color="black", linewidth=1, linetype=2)
+        f1 + geom_abline(slope = 1, intercept = 4, color = "black", 
+                         linewidth = 1, linetype = 2)
       } else {
-        f1 + stat_function(fun = function(x) (x^2)/2 + 4, linewidth=1, color="black", linetype=2)
+        f1 + stat_function(
+          fun = function(x) (x ^ 2) / 2 + 4,
+          linewidth = 1,
+          color = "black",
+          linetype = 2)
       }
       
-    }, height = 600, width = 900)
+    }, height = 300, width = 2 * 300)
   }
 
-getData <- function(DATA=inputData, 
-                    PCTMISSING=inputPctMissing,
-                    IMPUTETYPE=inputImputeType) {
-
+getData <- function(DATA = inputData,
+                    PCTMISSING = inputPctMissing,
+                    IMPUTETYPE = inputImputeType) {
+  
   linear_partition <- ifelse(DATA == "LINEAR", "TRUE", "FALSE")
   if(linear_partition){
     current_data <-
@@ -117,46 +125,40 @@ getData <- function(DATA=inputData,
   
   if(IMPUTETYPE == "BAGIMPUTE"){
     current_recipe <- current_recipe %>% step_impute_bag(all_predictors())
-  }
-  if(IMPUTETYPE == "KNNIMPUTE"){
+  } else if(IMPUTETYPE == "KNNIMPUTE"){
     current_recipe <- current_recipe %>% step_impute_knn(all_predictors())
-  }
-  if(IMPUTETYPE == "MEANIMPUTE"){
+  } else if(IMPUTETYPE == "MEANIMPUTE"){
     current_recipe <- current_recipe %>% step_impute_mean(all_predictors())
-  }
-  if(IMPUTETYPE == "MEDIANIMPUTE"){
+  } else if(IMPUTETYPE == "MEDIANIMPUTE"){
     current_recipe <- current_recipe %>% step_impute_median(all_predictors())
-  }
-  if(IMPUTETYPE == "LINEARIMPUTE"){
+  } else if(IMPUTETYPE == "LINEARIMPUTE"){
     current_recipe <- current_recipe %>% step_impute_linear(all_predictors())
   }
   
-  current_recipe <-
-    current_recipe %>%
-    prep(training = current_data, retain = TRUE)
-  
+  current_recipe <- prep(current_recipe, training = current_data, retain = TRUE)
   impute_data <- bake(current_recipe, current_data)
-  impute_data <- impute_data %>%
-    rename(x1 = x1_new,
-           x2 = x2_new) %>%
-    mutate(Type = "Imputed")
-  impute_data <- impute_data[rows_missing,]
   
-  orig_data <- current_data %>%
+  impute_data <- 
+    impute_data %>%
+    rename(x1 = x1_new, x2 = x2_new) %>%
+    mutate(Type = "Imputed") %>% 
+    dplyr::slice(rows_missing)
+  
+  orig_data <- 
+    current_data %>%
     mutate(Type = "Original") %>%
-    select(x1, x2, Observed, Type)
-  orig_data <- orig_data[rows_missing,]
+    dplyr::select(x1, x2, Observed, Type) %>% 
+    dplyr::slice(rows_missing)
   
-  for_figure <- bind_rows(orig_data, impute_data)
-  for_figure <- for_figure %>%
-    mutate(Obs_Type = factor(paste0(Observed, ": ", Type), 
-                             levels=c("Group1: Original", "Group2: Original", "Group1: Imputed", "Group2: Imputed")),
-           Type = factor(Type, levels=c("Original", "Imputed")))
+  groups <- c("Group1: Original", "Group2: Original", "Group1: Imputed", "Group2: Imputed")
+  for_figure <- 
+    bind_rows(orig_data, impute_data)  %>%
+    mutate(Obs_Type = factor(paste0(Observed, ": ", Type), levels = groups),
+           Type = factor(Type, levels = c("Original", "Imputed")))
   
   list(for_figure = for_figure,
        xlims = xlims,
        ylims = ylims,
        linear_partition = linear_partition)
 }
-
 app <- shinyApp(ui = ui, server = server)
