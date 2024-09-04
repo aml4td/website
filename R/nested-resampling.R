dbf06a06279c17dd3fd360a9516e2fce2fba2407
topepo <mxkuhn@gmail.com>
2024-09-04 18:58:21 -0400

    reorganize nested resampling results

--- R/nested-resampling.R
+++ R/nested-resampling.R
@@ -6,7 +6,7 @@ if ( packageVersion("finetune") < "1.2.0.9000" ) {
   cli::cli_abort("Please load the devel branch of finetune using \\
                  {.code pak::pak('tidymodels/finetune@nested')}")
 }
-library(doMC)
+library(future)
 
 # ------------------------------------------------------------------------------
 
@@ -14,11 +14,12 @@ tidymodels_prefer()
 theme_set(theme_bw())
 options(pillar.advice = FALSE, pillar.min_title_chars = Inf)
 
+plan(multisession)
 num_workers <- parallel::detectCores()
 
 # ------------------------------------------------------------------------------
 
-source("~/content/website/R/setup_two_class_example.R")
+source("~/github/website/R/setup_two_class_example.R")
 
 set.seed(943)
 sim_tr <- sim_logistic(2000, sim_f)
@@ -31,181 +32,88 @@ sim_nested_rs <- nested_cv(sim_tr, outside = vfold_cv(), inside = vfold_cv())
 
 # ------------------------------------------------------------------------------
 
-bst_spec <- 
-  boost_tree(learn_rate = tune(), trees = tune()) %>% 
+one_dim_spec <- 
+  boost_tree(learn_rate = tune(), trees = 500) %>% 
   set_mode("classification") %>% 
   set_engine("lightgbm")
 
-bst_wflow <-
+one_dim_wflow <-
   workflow() %>%
-  add_model(bst_spec) %>%
+  add_model(one_dim_spec) %>%
   add_formula(class ~ A + B)
 
-bst_param <- 
-  bst_wflow %>% 
+one_dim_param <- 
+  one_dim_wflow %>% 
   extract_parameter_set_dials() %>% 
   update(learn_rate = learn_rate(c(-3, -1)))
 
-bst_reg <-
-  grid_regular(bst_param, levels = c(5, 3)) %>%
-  mutate(grid = "Regular")
-
-sfd_size <- nrow(bst_reg)
-
-set.seed(nrow(bst_reg))
-bst_random <-
-  grid_random(bst_param, size = sfd_size) %>%
-  mutate(grid = "Random")
-
-bst_sfd <- grid_space_filling(bst_param, size = sfd_size)
-
-large_param <- 
-  bst_param %>% 
-  update(
-    trees = trees(c(1, 3000)),
-    learn_rate = learn_rate(c(-5, -1))
-  )
-
-large_sfd <- 
-  large_param %>% 
-  grid_space_filling(size = 100)
-
-large_reg <- 
-  large_param %>% 
-  grid_regular(levels = 10)
+one_dim_reg <- grid_regular(one_dim_param, levels = 100) 
 
 # ------------------------------------------------------------------------------
 
-sfd_seq_time <- 
+grid_time <- 
   system.time({
     set.seed(1)
-    tmp <-
-      bst_wflow %>%
+    grid_res <-
+      one_dim_wflow %>%
       tune_grid(
         resamples = sim_rs,
         metrics = metric_set(brier_class),
-        grid = bst_sfd
+        grid = one_dim_reg
       )
   })
 
-registerDoMC(cores = num_workers)
-
-sfd_time <- 
+grid_nest_time <- 
   system.time({
     set.seed(1)
-    sfd_res <-
-      bst_wflow %>%
-      tune_grid(
-        resamples = sim_rs,
-        metrics = metric_set(brier_class),
-        grid = bst_sfd
-      )
-  })
-
-
-sfd_nest_time <- 
-  system.time({
-    set.seed(1)
-    sfd_nest_res <-
-      bst_wflow %>%
+    nest_grid_res <-
+      one_dim_wflow %>%
       tune_nested(
         resamples = sim_nested_rs,
         metrics = metric_set(brier_class),
-        grid = bst_sfd
+        grid = one_dim_reg
       )
   })
 
-# TODO not naming an argument gets an error of :
-# Caused by error in `rlang::call_modify()`:
-#   ! argument "arg" is missing, with no default
+bo_time <- 
+  system.time({
+    set.seed(1)
+    bo_res <-
+      one_dim_wflow %>%
+      tune_bayes(
+        resamples = sim_rs,
+        metrics = metric_set(brier_class),
+        initial = 3, 
+        iter = 10,
+        param_info = one_dim_param
+      ) 
+  })
+
+bo_nest_time <- 
+  system.time({
+    set.seed(1)
+    nest_bo_res <-
+      one_dim_wflow %>%
+      tune_nested(
+        resamples = sim_nested_rs,
+        metrics = metric_set(brier_class),
+        fn = "tune_bayes",
+        initial = 3, 
+        iter = 20,
+        param_info = one_dim_param
+      )
+  })
 
 # ------------------------------------------------------------------------------
 
-large_grid_time <- 
-  system.time({
-    set.seed(1)
-    large_grid_res <-
-      bst_wflow %>%
-      tune_grid(
-        resamples = sim_rs,
-        metrics = metric_set(brier_class),
-        grid = large_sfd
-      )
-  })
-
-
-large_grid_reg_time <- 
-  system.time({
-    set.seed(1)
-    large_grid_reg_res <-
-      bst_wflow %>%
-      tune_grid(
-        resamples = sim_rs,
-        metrics = metric_set(brier_class),
-        grid = large_reg
-      )
-  })
-
-
-large_nest_grid_time <- 
-  system.time({
-    set.seed(1)
-    large_nest_grid_res <-
-      bst_wflow %>%
-      tune_nested(
-        resamples = sim_nested_rs,
-        metrics = metric_set(brier_class),
-        grid = large_sfd
-      )
-  })
-
-
-large_race_time <- 
-  system.time({
-    set.seed(1)
-    large_race_res <-
-      bst_wflow %>% 
-      tune_race_anova(
-        resamples = sim_rs,
-        metrics = metric_set(brier_class),
-        grid = large_sfd,
-        control = control_race(verbose_elim = FALSE)
-      ) 
-  })
-
-large_nest_race_time <- 
-  system.time({
-    set.seed(1)
-    bst_race_nest_res <-
-      bst_wflow %>%
-      tune_nested(
-        resamples = sim_nested_rs,
-        metrics = metric_set(brier_class),
-        fn = "tune_race_anova",
-        grid = large_sfd
-      )
-  })
-
-registerDoSEQ()
-
-large_seq_time <- 
-  system.time({
-    set.seed(1)
-    large_race_tmp <-
-      bst_wflow %>% 
-      tune_race_anova(
-        resamples = sim_rs,
-        metrics = metric_set(brier_class),
-        grid = large_sfd,
-        control = control_race(verbose_elim = FALSE)
-      ) 
-  })
+debiased_grid <- debias_estimate(grid_res)
+debiased_bo <- debias_estimate(bo_res)
 
 # ------------------------------------------------------------------------------
 
 save(list = c(ls(pattern = "_res$"), ls(pattern = "_time$"), 
-              "large_param", "large_sfd", "num_workers"),
-     file = "~/content/website/RData/nested_res.RData")
+              "debiased_grid", "debiased_bo", "num_workers"),
+     file = "~/github/website/RData/nested_res.RData")
 
 # ------------------------------------------------------------------------------
 
