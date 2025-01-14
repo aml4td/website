c598a6f9ef5732fef319fc0046067e6ea42ef240
‘topepo’ <mxkuhn@gmail.com>
2025-01-13 20:42:27 -0500

    updates

--- R/qc_qmd.R
+++ R/qc_qmd.R
@@ -3,8 +3,9 @@
 # - titles but not in title case
 # - no fancy quotes
 
-# md linters can flag a lot more, such as
-# https://github.com/DavidAnson/markdownlint/tree/v0.37.3
+
+# See this repo for a much more sophisticated and rigorour approach: 
+# https://github.com/DavidAnson/markdownlint
 
 get_title <- function(txt) {
   orig <- txt
@@ -19,10 +20,6 @@ get_title <- function(txt) {
   fixed
 }
 
-# get_title("### Overall Comparisons")
-# get_title("## Linear Transformations  {#sec-linear-embed}")
-# get_title("#### How to incorporate PCA into the model {.unnumbered}")
-
 needs_stub <- function(txt) {
   is_section <- grepl("(^#{2}.+)[a-zA-z]", txt)
   has_stub <- grepl("{#sec-", txt, fixed = TRUE)
@@ -32,14 +29,16 @@ needs_stub <- function(txt) {
 }
 
 qc_qmd <- function(path) {
+  suppressPackageStartupMessages(library(dplyr))
+  
   content <- readLines(path)
-  cli::cli_inform(c(i = "processing {.val {basename(path)}}"))
-  # check apostrophies
+  file_nm <- basename(path)
+
+  # check apostrophes
   content <- gsub("(”)|(“)", "\"", content)
   
   # check data are plural
-  
-  # mutiple blank lines
+  # TODO
 
   # Section checks and fixes
   is_section <- grepl("(^#{2}.+) [a-zA-z]", content)
@@ -49,8 +48,28 @@ qc_qmd <- function(path) {
     content[i] <- get_title(content[i])
     ## check that there is a stub for each section
     if (needs_stub(content[i])) {
-      cli::cli_inform(c(x = "Line {i} needs a stub: {.val {content[i]}}"))
+      cli::cli_inform(c(x = "Needs a stub: {.val {content[i]}} at {.file {path}:{i}}"))
+      content[i] <- paste(content[i], "{#sec-validation}")
     }
+  }
+  
+  # mutiple blank lines
+  blank_line <- grepl("^\\s*$", content)
+  blank_line <- which(blank_line)
+  blank_diff <- diff(blank_line)
+  if (any(blank_diff == 1)) {
+    cli::cli_inform("There are some replicate blank lines")
+    blank_start <- blank_line[blank_diff == 1]
+    removals <- integer(0)
+    for (blank_i in blank_start) {
+      cat(cli::rule(), "\n")
+      cli::cli_inform(c(x = "Extra blank line at {.file {path}:{blank_i}}:"))
+      cat(content[(blank_i-2):(blank_i+2)], sep = "\n")
+      removals <- c(removals, blank_i)
+      cat(cli::rule(), "\n")
+    }
+    removals <- unique(removals)
+    content <- content[-removals]
   }
   
   # write out results
@@ -61,5 +80,3 @@ qc_qmd <- function(path) {
 qmd_files <- list.files(path = "chapters", pattern = "\\.qmd$", full.names = TRUE)
 qmd_files <- qmd_files[!grepl("(news)|(contri)", qmd_files)]
 linted <- purrr::map_lgl(qmd_files, qc_qmd)
-
-
