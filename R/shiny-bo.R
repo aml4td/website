282c1074ce3fff363f8850add2c4b6291c70476d
Max Kuhn <mxkuhn@gmail.com>
2025-04-16 10:00:12 -0400

    Shiny cleanup part 1 (#81)
    * air format, url function
    
    * move poly/spline code to external files
    
    * note about shiny-setup.R
    
    * explicitly return shiny app

--- R/shiny-bo.R
+++ R/shiny-bo.R
@@ -1,123 +1,143 @@
+# Requires the sources in shiny-setup.R
+
 ui <- page_fillable(
-  theme = bs_theme(bg = "#fcfefe", fg = "#595959"),
-  sliderInput(
-    "iter",
-    label = "Iteration",
-    min = 1L,
-    max = 50L,
-    step = 1L,
-    value = 1L,
-    width = "100%"
-  ),
-  radioButtons(
-    inputId = "objective",
-    label = "Objective Function",
-    inline = TRUE,
-    choices = list("Exp. Improivement" = "objective", "Mean" = ".mean", 
-                   "Std. Dev" = ".sd")
-  ),
-  as_fill_carrier(plotOutput("path"))
+	theme = grid_theme,
+	sliderInput(
+		"iter",
+		label = "Iteration",
+		min = 1L,
+		max = 50L,
+		step = 1L,
+		value = 1L,
+		width = "100%"
+	),
+	radioButtons(
+		inputId = "objective",
+		label = "Objective Function",
+		inline = TRUE,
+		choices = list(
+			"Exp. Improivement" = "objective",
+			"Mean" = ".mean",
+			"Std. Dev" = ".sd"
+		)
+	),
+	as_fill_carrier(plotOutput("path"))
 )
 
 server <- function(input, output) {
-  
-  load(url("https://raw.githubusercontent.com/aml4td/website/main/RData/two_param_iter_bo.RData"))
-  load(url("https://raw.githubusercontent.com/aml4td/website/main/RData/two_param_iter_large.RData"))
-  
-  # ------------------------------------------------------------------------------
-  
-  x_rng <- 10^extendrange(c(-10, -1/10))
-  y_rng <- 2^extendrange(c(-10, 10))
-  
-  log10_labs <- trans_format("log10", math_format(10^.x, function(x) format(x, digits = 3)))
-  log2_labs <- trans_format("log2", math_format(2^.x, function(x) format(x, digits = 3)))
-  
-  # ------------------------------------------------------------------------------
-  
-  best_init <- 
-    bo_mtr %>% 
-    filter(.iter == 0) %>% 
-    slice_min(mean) %>% 
-    select(cost, scale_factor, .iter)
-  
-  initial_plot <- 
-    regular_mtr %>%
-    ggplot(aes(scale_factor, cost)) +
-    geom_point(data = init_grid, cex = 3, pch = 1) +
-    scale_x_log10(limits = x_rng,
-                  labels = log10_labs,
-                  expand = expansion(add = c(-1 / 5, -1 / 5))) +
-    scale_y_continuous(limits = y_rng, trans = "log2", labels = log2_labs,
-                       expand = expansion(add = c(-1/2, -1/2))) +
-    theme_bw() +
-    labs(x = "Scaling Factor", y = "Cost") +
-    coord_fixed(ratio = 1/2)
-  
-  bo_tile <- tidyr::pivot_longer(
-    bo_tile,
-    cols = c(.mean, .sd, objective),
-    names_to = "metric",
-    values_to = "value"
-  )
-  
-  # ------------------------------------------------------------------------------
-  
-  output$path <-
-    renderPlot({
-      
-      tile <- bo_tile %>% filter(.iter == input$iter & metric == input$objective)
-      last_best <- bo_mtr %>% filter(.iter < input$iter) %>% slice_min(mean)
-      path <- 
-        bo_mtr %>% 
-        filter(.iter <= input$iter & .iter > 0) %>% 
-        select(cost, scale_factor, .iter) %>% 
-        bind_rows(best_init) %>% 
-        arrange(.iter)
-      
-      # TODO convert to discrete colors via cut
-      rc_pal <- switch(
-        input$objective,
-        "objective" = "Greens",
-        ".mean" = "OrRd",
-        ".sd" = "Blues"
-      )
-      
-      hist_alpha <- 
-        case_when(
-          input$iter < 5 ~ 1/2, 
-          input$iter >= 5 & input$iter < 10 ~ 1/3,  
-          input$iter >= 10 & input$iter < 20 ~ 1/4, 
-          input$iter >= 20 & input$iter < 30 ~ 1/5, 
-          TRUE ~ 1 / 10)
-      hist_alpha <- 2 * hist_alpha
-      
-      col_dir <- if_else(input$objective == ".mean", -1, 1)
-      col_tr <- "identity"
-      
-      current_plot <- 
-        initial_plot + 
-        geom_tile(data = tile, aes(fill = value), alpha = 1 / 2) +
-        geom_line(
-          data = regular_mtr %>% slice_min(mean, n = 18),
-          stat = "smooth",
-          col = "black",
-          method = lm,
-          se = FALSE,
-          formula = y ~ x,
-          alpha = 1 / 4,
-          linewidth = 2
-        ) +
-        geom_path(data = path, col = "black", alpha = hist_alpha, lty = 3) +
-        geom_point(data = new_bo_points %>% filter(iter == input$iter), 
-                   col = "black", pch = 16, cex = 4) +
-        geom_point(data = last_best, cex = 4, col = "black", pch = 8) + 
-        scale_fill_distiller(palette = rc_pal, type = "seq", 
-                             direction = col_dir, transform = col_tr) +
-        theme(legend.title = element_blank(), legend.text = element_blank())
-      print(current_plot)
-      
-    }, res = 100)
-  
+  load(rd_url("two_param_iter_bo.RData"))
+  load(rd_url("two_param_iter_large.RData"))
+
+	# ------------------------------------------------------------------------------
+
+	x_rng <- 10^extendrange(c(-10, -1 / 10))
+	y_rng <- 2^extendrange(c(-10, 10))
+
+	log10_labs <- trans_format(
+		"log10",
+		math_format(10^.x, function(x) format(x, digits = 3))
+	)
+	log2_labs <- trans_format(
+		"log2",
+		math_format(2^.x, function(x) format(x, digits = 3))
+	)
+
+	# ------------------------------------------------------------------------------
+
+	best_init <- bo_mtr %>%
+		filter(.iter == 0) %>%
+		slice_min(mean) %>%
+		select(cost, scale_factor, .iter)
+
+	initial_plot <- regular_mtr %>%
+		ggplot(aes(scale_factor, cost)) +
+		geom_point(data = init_grid, cex = 3, pch = 1) +
+		scale_x_log10(
+			limits = x_rng,
+			labels = log10_labs,
+			expand = expansion(add = c(-1 / 5, -1 / 5))
+		) +
+		scale_y_continuous(
+			limits = y_rng,
+			trans = "log2",
+			labels = log2_labs,
+			expand = expansion(add = c(-1 / 2, -1 / 2))
+		) +
+		theme_bw() +
+		labs(x = "Scaling Factor", y = "Cost") +
+		coord_fixed(ratio = 1 / 2)
+
+	bo_tile <- tidyr::pivot_longer(
+		bo_tile,
+		cols = c(.mean, .sd, objective),
+		names_to = "metric",
+		values_to = "value"
+	)
+
+	# ------------------------------------------------------------------------------
+
+	output$path <- renderPlot(
+		{
+			tile <- bo_tile %>%
+				filter(.iter == input$iter & metric == input$objective)
+			last_best <- bo_mtr %>% filter(.iter < input$iter) %>% slice_min(mean)
+			path <- bo_mtr %>%
+				filter(.iter <= input$iter & .iter > 0) %>%
+				select(cost, scale_factor, .iter) %>%
+				bind_rows(best_init) %>%
+				arrange(.iter)
+
+			# TODO convert to discrete colors via cut
+			rc_pal <- switch(
+				input$objective,
+				"objective" = "Greens",
+				".mean" = "OrRd",
+				".sd" = "Blues"
+			)
+
+			hist_alpha <- case_when(
+				input$iter < 5 ~ 1 / 2,
+				input$iter >= 5 & input$iter < 10 ~ 1 / 3,
+				input$iter >= 10 & input$iter < 20 ~ 1 / 4,
+				input$iter >= 20 & input$iter < 30 ~ 1 / 5,
+				TRUE ~ 1 / 10
+			)
+			hist_alpha <- 2 * hist_alpha
+
+			col_dir <- if_else(input$objective == ".mean", -1, 1)
+			col_tr <- "identity"
+
+			current_plot <- initial_plot +
+				geom_tile(data = tile, aes(fill = value), alpha = 1 / 2) +
+				geom_line(
+					data = regular_mtr %>% slice_min(mean, n = 18),
+					stat = "smooth",
+					col = "black",
+					method = lm,
+					se = FALSE,
+					formula = y ~ x,
+					alpha = 1 / 4,
+					linewidth = 2
+				) +
+				geom_path(data = path, col = "black", alpha = hist_alpha, lty = 3) +
+				geom_point(
+					data = new_bo_points %>% filter(iter == input$iter),
+					col = "black",
+					pch = 16,
+					cex = 4
+				) +
+				geom_point(data = last_best, cex = 4, col = "black", pch = 8) +
+				scale_fill_distiller(
+					palette = rc_pal,
+					type = "seq",
+					direction = col_dir,
+					transform = col_tr
+				) +
+				theme(legend.title = element_blank(), legend.text = element_blank())
+			print(current_plot)
+		},
+		res = 100
+	)
 }
 
 app <- shinyApp(ui, server)
---
+++ R/shiny-piecewise.R
@@ -0,0 +1,155 @@
+# Requires the sources in shiny-setup.R
+
+data(fossil)
+
+ui <- page_fillable(
+	theme = grid_theme,
+	padding = "1rem",
+	layout_columns(
+		fill = FALSE,
+		col_widths = breakpoints(sm = c(-1, 5, 5, -1)),
+		sliderInput(
+			"piecewise_deg",
+			label = "Polynomial Degree",
+			min = 0L,
+			max = 6L,
+			step = 1L,
+			value = 4L
+		), # sliderInput
+		sliderInput(
+			"cuts",
+			label = "Cutpoints",
+			min = 93L,
+			max = 122L,
+			step = 1,
+			value = c(101, 118)
+		) # sliderInput
+	), # layout_columns
+	layout_columns(
+		fill = FALSE,
+		col_widths = breakpoints(sm = c(-1, 10, -1)),
+		as_fill_carrier(plotOutput('pieces'))
+	)
+)
+
+server <- function(input, output, session) {
+	maybe_lm <- function(x) {
+		try(lm(y ~ poly(x, input$piecewise_deg), data = x), silent = TRUE)
+	}
+
+	expansion_to_tibble <- function(x, original, prefix = "term ") {
+		cls <- class(x)[1]
+		nms <- names_zero_padded(ncol(x), prefix)
+		colnames(x) <- nms
+		x <- as_tibble(x)
+		x$variable <- original
+		res <- tidyr::pivot_longer(x, cols = c(-variable))
+		if (cls != "poly") {
+			res <- res[res$value > .Machine$double.eps, ]
+		}
+		res
+	}
+
+	mult_poly <- function(dat, degree = 4) {
+		rng <- extendrange(dat$x, f = .025)
+		grid <- seq(rng[1], rng[2], length.out = 1000)
+		grid_df <- tibble(x = grid)
+		feat <- poly(grid_df$x, degree)
+		res <- expansion_to_tibble(feat, grid_df$x)
+
+		# make some random names so that we can plot the features with distinct colors
+		rand_names <- lapply(
+			1:degree,
+			function(x) paste0(sample(letters)[1:10], collapse = "")
+		)
+		rand_names <- unlist(rand_names)
+		rand_names <- tibble(name = unique(res$name), name2 = rand_names)
+		res <- dplyr::inner_join(res, rand_names, by = dplyr::join_by(name)) %>%
+			dplyr::select(-name) %>%
+			dplyr::rename(name = name2)
+		res
+	}
+
+	# ------------------------------------------------------------------------------
+
+	spline_example <- tibble(x = fossil$age, y = fossil$strontium.ratio)
+	rng <- extendrange(fossil$age, f = .025)
+	grid <- seq(rng[1], rng[2], length.out = 1000)
+	grid_df <- tibble(x = grid)
+	alphas <- 1 / 4
+	line_wd <- 1.0
+
+	base_p <- spline_example %>%
+		ggplot(aes(x = x, y = y)) +
+		geom_point(alpha = 3 / 4, pch = 1, cex = 3) +
+		labs(x = "Age", y = "Isotope Ratio") +
+		theme_light_bl()
+
+	output$pieces <- renderPlot({
+		cuts <- c(0, sort(input$cuts), 60)
+		piece_cols <- c("#1B9E77", "#D95F02", "#7570B3")
+		piece_p <- base_p
+
+		if (input$piecewise_deg > 0) {
+			data_splt <- spline_example %>%
+				dplyr::mutate(x_cut = cut(x, breaks = cuts, include.lowest = TRUE)) %>%
+				tidyr::nest(.by = x_cut) %>%
+				mutate(
+					fit = lapply(data, maybe_lm),
+					features = lapply(data, mult_poly, degree = input$piecewise_deg)
+				)
+			grid_splt <- dplyr::tibble(x = grid) %>%
+				dplyr::mutate(x_cut = cut(x, breaks = cuts, include.lowest = TRUE)) %>%
+				tidyr::nest(.by = x_cut)
+
+			for (i in 1:3) {
+				sub_pred <- grid_splt$data[[i]]
+				if (!inherits(data_splt$fit[[i]], "try-error")) {
+					sub_pred <- sub_pred %>%
+						dplyr::bind_cols(
+							predict(
+								data_splt$fit[[i]],
+								sub_pred,
+								interval = "confidence",
+								level = .90
+							)
+						)
+
+					piece_p <- piece_p +
+						geom_ribbon(
+							data = sub_pred,
+							aes(y = NULL, ymin = lwr, ymax = upr),
+							alpha = 1 / 15
+						) +
+						geom_line(
+							data = sub_pred,
+							aes(y = fit),
+							linewidth = line_wd
+						)
+				}
+			}
+
+			set.seed(383) # to control colors
+			feature_p <- data_splt %>%
+				dplyr::select(features) %>%
+				tidyr::unnest(features) %>%
+				ggplot(aes(x = variable, y = value, col = name)) +
+				geom_line(show.legend = FALSE) +
+				theme_void() +
+				theme(
+					plot.margin = margin(t = 0, r = 0, b = -20, l = 0),
+					panel.background = col_rect,
+					plot.background = col_rect,
+					legend.background = col_rect,
+					legend.key = col_rect
+				) +
+				scale_color_viridis(discrete = TRUE, option = "turbo")
+
+			p <- (feature_p / piece_p) + plot_layout(heights = c(1, 4))
+		}
+
+		print(p)
+	})
+}
+
+app <- shinyApp(ui, server)
