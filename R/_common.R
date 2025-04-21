462a91631906bd8a424b9aaeecbd21e48339a35a
topepo <mxkuhn@gmail.com>
2025-04-21 15:10:38 -0400

    changes for tables

--- R/_common.R
+++ R/_common.R
@@ -11,6 +11,7 @@ teardown <- TRUE
 # ------------------------------------------------------------------------------
 
 light_bg <- "#fcfefe" # from aml4td.scss
+dark_bg <- "#222" 
 
 # ------------------------------------------------------------------------------
 # ggplot stuff
@@ -57,6 +58,133 @@ dk_gif_thm <-
     plot.background = ggplot2::element_rect(fill = "#222", color = NA),
     strip.background = dk_rect
   )
+
+# ------------------------------------------------------------------------------
+
+lt_tbl_thm <- function(gt_object, ...) {
+  gt_object %>%
+    gt::tab_options(
+      table.background.color = "#fcfefe",
+      table.font.color.light = "grey",
+      table.border.left.color = "#fcfefe",
+      table.border.right.color = "#fcfefe",
+      table_body.border.bottom.color = "grey",
+      table_body.border.top.color = "grey",
+      column_labels.background.color = "#fcfefe",
+      data_row.padding = gt::px(7),
+      ...
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_text(
+        color = "#595959",
+        font = gt::google_font("Libre Franklin"),
+      ),
+      locations = gt::cells_column_labels()
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_text(
+        color = "#595959",
+        font = gt::google_font("Libre Franklin"),
+      ),
+      locations = gt::cells_body()
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_text(
+        color = "#595959",
+        font = gt::google_font("Libre Franklin"),
+      ),
+      locations = gt::cells_column_spanners()
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_text(
+        font = gt::google_font("Libre Franklin"),
+        weight = 800,
+        color = "#595959"
+      ),
+      locations = gt::cells_title(groups = "title")
+    ) |> 
+    gt::tab_style(
+      style = gt::cell_borders(sides = "bottom", weight = gt::px(3), col = "grey"),
+      locations = list(
+        gt::cells_column_labels()
+      )
+    )  |> 
+    gt::tab_style(
+      style = gt::cell_borders(
+        sides = c("top", "bottom"),
+        color = "grey",
+        weight = gt::px(1),
+        style = "solid"
+      ),
+      locations = gt::cells_body()
+    )
+}
+
+dk_tbl_thm <- function(gt_object, ...) {
+  gt_object %>%
+    gt::tab_options(
+      table_body.border.bottom.color = "#F2EFE5",
+      table_body.border.top.color = "#F2EFE5",
+      table.border.bottom.color = "#F2EFE5",
+      table.border.top.color = "#F2EFE5",
+      table.background.color = "#222",
+      table.font.color.light = "#F2EFE5",
+      table.border.left.color = "#222",
+      table.border.right.color = "#222",
+      column_labels.background.color = "#222",
+      data_row.padding = gt::px(7),
+      ...
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_text(
+        color = "#adb5bd",
+        font = gt::google_font("Libre Franklin"),
+      ),
+      locations = gt::cells_column_labels()
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_text(
+        color = "#adb5bd",
+        font = gt::google_font("Libre Franklin"),
+      ),
+      locations = gt::cells_body()
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_text(
+        color = "#adb5bd",
+        font = gt::google_font("Libre Franklin"),
+      ),
+      locations = gt::cells_column_spanners()
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_text(
+        font = gt::google_font("Libre Franklin"),
+        weight = 800,
+        color = "#adb5bd"
+      ),
+      locations = gt::cells_title(groups = "title")
+    ) |>
+    gt::tab_style(
+      style = gt::cell_borders(
+        sides = c("top", "bottom"),
+        weight = gt::px(3),
+        col = "#F2EFE5"
+      ),
+      locations = list(
+        gt::cells_column_labels(), 
+        gt::cells_column_spanners()
+      )
+    ) |>
+    gt::tab_style(
+      style = gt::cell_borders(
+        sides = c("top", "bottom"),
+        color = "#F2EFE5",
+        weight = gt::px(1),
+        style = "solid"
+      ),
+      locations = gt::cells_body()
+    )
+}
 
 # ------------------------------------------------------------------------------
 # formatting for package names
