44d8726f536511631f39c459f445e6f29b5d5858
‘topepo’ <mxkuhn@gmail.com>
2025-08-18 12:03:28 -0400

    remove library calls and themes

--- R/shiny-penalty.R
+++ R/shiny-penalty.R
@@ -1,29 +1,3 @@
-library(shiny)
-library(bslib)
-library(dplyr)
-library(ggplot2)
-library(scales)
-
-light_bg <- "#fcfefe" # from aml4td.scss
-grid_theme <- bs_theme(
-  bg = light_bg,
-  fg = "#595959"
-)
-
-theme_light_bl <- function(...) {
-  ret <- ggplot2::theme_bw(...)
-  
-  col_rect <- ggplot2::element_rect(fill = light_bg, colour = light_bg)
-  ret$panel.background <- col_rect
-  ret$plot.background <- col_rect
-  ret$legend.background <- col_rect
-  ret$legend.key <- col_rect
-  
-  ret$legend.position <- "top"
-  
-  ret
-}
-
 
 ui <- fluidPage(
   theme = grid_theme,
@@ -113,5 +87,3 @@ server <- function(input, output) {
 }
 
 app <- shinyApp(ui = ui, server = server)
-
-app
