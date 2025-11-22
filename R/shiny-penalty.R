3a8467c7d0c2d890c2d81a38080cd7b5463f6b41
‘topepo’ <mxkuhn@gmail.com>
2025-11-22 11:20:48 -0500

    allow mixture = 0 + air format

--- R/shiny-penalty.R
+++ R/shiny-penalty.R
@@ -7,13 +7,13 @@ ui <- fluidPage(
       sliderInput(
         inputId = "mixture",
         label = "Lasso Proportion (glmnet only)",
-        min = 0.1,
+        min = 0.0,
         max = 1.0,
         value = 0.6,
         width = "100%",
         step = 0.1
       )
-    ), 
+    ),
     column(
       width = 5,
       sliderInput(
@@ -25,8 +25,8 @@ ui <- fluidPage(
         width = "100%",
         step = 1
       )
-    ), 
-    
+    ),
+
     fluidRow(
       column(
         width = 3,
@@ -58,31 +58,45 @@ server <- function(input, output) {
   ))
 
   output$plot <-
-    renderPlot({
-      dat <- all_penalties |> dplyr::filter(method == input$method)
-      if (input$method == "glmnet") {
-        dat <- dat |> dplyr::filter(mixture == input$mixture)
-      } else if (input$method %in% c("SCAD", "MCP")) {
-        dat <- dat |> dplyr::filter(gamma == input$gamma)
-      }
-        
-      p <-
-        dat |> 
-        ggplot(aes(penalty, estimate, col = Term, pch = Term))  +
-        geom_hline(yintercept = 0 , col = "red", linewidth = 1.1, alpha = 1 / 8) +
-        geom_hline(data = mle_est, aes(yintercept = estimate, col = Term), 
-                   lty = 2, alpha = 3 / 4) +
-        geom_line(alpha = 1 / 2) +
-        geom_point(alpha = 1 / 2) +
-        scale_x_log10(labels = label_log(), breaks = scales:: trans_breaks("log10", function(x) 10^x)) +
-        scale_color_manual(values = c("#3381A8FF", "#5E9546FF")) +
-        labs(x = "Penalty", y = "Parameter Estimate") +
-        theme_bw() +
-        theme(legend.position = "top")
-      
-      print(p)
-    }, 
-    res = 100)
+    renderPlot(
+      {
+        dat <- all_penalties |> dplyr::filter(method == input$method)
+        if (input$method == "glmnet") {
+          dat <- dat |> dplyr::filter(mixture == input$mixture)
+        } else if (input$method %in% c("SCAD", "MCP")) {
+          dat <- dat |> dplyr::filter(gamma == input$gamma)
+        }
+
+        p <-
+          dat |>
+          ggplot(aes(penalty, estimate, col = Term, pch = Term)) +
+          geom_hline(
+            yintercept = 0,
+            col = "red",
+            linewidth = 1.1,
+            alpha = 1 / 8
+          ) +
+          geom_hline(
+            data = mle_est,
+            aes(yintercept = estimate, col = Term),
+            lty = 2,
+            alpha = 3 / 4
+          ) +
+          geom_line(alpha = 1 / 2) +
+          geom_point(alpha = 1 / 2) +
+          scale_x_log10(
+            labels = label_log(),
+            breaks = scales::trans_breaks("log10", function(x) 10^x)
+          ) +
+          scale_color_manual(values = c("#3381A8FF", "#5E9546FF")) +
+          labs(x = "Penalty", y = "Parameter Estimate") +
+          theme_bw() +
+          theme(legend.position = "top")
+
+        print(p)
+      },
+      res = 100
+    )
 }
 
 app <- shinyApp(ui = ui, server = server)
