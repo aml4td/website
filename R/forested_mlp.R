1bba579e5b73914ef9a81aff01dae9e7454d896e
Max Kuhn <mxkuhn@gmail.com>
2026-01-20 13:17:03 -0500

    Discriminant analysis (#105)
    * initial work on naive Bayes and FDA/MARS
    
    * mars and reference updates
    
    * fix nprune
    
    * more text
    
    * more edits
    
    * update bib
    
    * namespace selects
    
    * air format
    
    * comparisons between the neural network and FDA
    
    * small code updates
    
    * renv update
    
    * Apply suggestions from code review
    
    Co-authored-by: Kjell Johnson <kjell@stattenacity.com>
    
    ---------
    
    Co-authored-by: Kjell Johnson <kjell@stattenacity.com>

--- R/forested_mlp.R
+++ R/forested_mlp.R
@@ -22,30 +22,30 @@ encode_rec <-
   step_orderNorm(all_numeric_predictors())
 
 encode_pca_rec <-
-  encode_rec |> 
-  step_pca(all_numeric_predictors(), -county, threshold = 1)|>
+  encode_rec |>
+  step_pca(all_numeric_predictors(), -county, threshold = 1) |>
   step_orderNorm(starts_with("PC"))
 
 dummy_on_rec <-
   recipe(class ~ ., data = forested_train) |>
   step_dummy(county) |>
-  step_zv(all_predictors()) |> 
+  step_zv(all_predictors()) |>
   step_orderNorm(all_numeric_predictors())
 
 dummy_on_pca_rec <-
-  dummy_on_rec |> 
-  step_pca(all_numeric_predictors(), -starts_with("county"), threshold = 1)|>
+  dummy_on_rec |>
+  step_pca(all_numeric_predictors(), -starts_with("county"), threshold = 1) |>
   step_orderNorm(starts_with("PC"))
 
 dummy_cs_rec <-
   recipe(class ~ ., data = forested_train) |>
-  step_dummy(county) |> 
-  step_zv(all_predictors()) |> 
+  step_dummy(county) |>
+  step_zv(all_predictors()) |>
   step_normalize(all_numeric_predictors())
 
 dummy_cs_pca_rec <-
-  dummy_cs_rec |> 
-  step_pca(all_numeric_predictors(), -starts_with("county"), threshold = 1)|>
+  dummy_cs_rec |>
+  step_pca(all_numeric_predictors(), -starts_with("county"), threshold = 1) |>
   step_normalize(starts_with("PC"))
 
 # ------------------------------------------------------------------------------
@@ -100,12 +100,11 @@ mlp_wflow_set <-
       orderNorm_pca = dummy_on_pca_rec,
       plain_pca = dummy_cs_pca_rec
     ),
-    models = 
-      list(
-        `1L_AdamW` = mlp_adam_spec,
-        `2L_AdamW` = mlp_adam_2layer_spec
-      )
-  ) |> 
+    models = list(
+      `1L_AdamW` = mlp_adam_spec,
+      `2L_AdamW` = mlp_adam_2layer_spec
+    )
+  ) |>
   option_add_parameters()
 
 # ------------------------------------------------------------------------------
@@ -113,23 +112,24 @@ mlp_wflow_set <-
 pull_iter <- function(x) {
   require(tidymodels)
   fit <- extract_fit_engine(x)
-  tibble(epoch_actual = fit$best_epoch,
-         num_param = length(unlist(coef(fit))))
+  tibble(epoch_actual = fit$best_epoch, num_param = length(unlist(coef(fit))))
 }
 
-ctrl <- control_race(save_pred = TRUE,
-                     parallel_over = "everything",
-                     extract = pull_iter)
+ctrl <- control_race(
+  save_pred = TRUE,
+  parallel_over = "everything",
+  extract = pull_iter
+)
 
-mlp_wflow_set <- 
-  mlp_wflow_set |> 
+mlp_wflow_set <-
+  mlp_wflow_set |>
   option_add(control = ctrl)
 
 for (i in 1:nrow(mlp_wflow_set)) {
   wflow_id <- mlp_wflow_set$wflow_id[[i]]
-  
+
   prm <- mlp_wflow_set$option[[i]]$param_info
-  prm <- prm |> 
+  prm <- prm |>
     update(
       momentum = momentum(c(0.8, 0.99)),
       penalty = penalty(c(-10, -1)),
@@ -137,51 +137,51 @@ for (i in 1:nrow(mlp_wflow_set)) {
       activation = activation(c("elu", "relu", "tanh", "tanhshrink")),
       rate_schedule = rate_schedule(c("cyclic", "decay_time", "none"))
     )
-  mlp_wflow_set$option[[i]]$param_info <- prm   
+  mlp_wflow_set$option[[i]]$param_info <- prm
 }
 
 # ------------------------------------------------------------------------------
 
 set.seed(12)
-mlp_grid_res <- 
-  mlp_wflow_set |> 
+mlp_grid_res <-
+  mlp_wflow_set |>
   workflow_map(
-    resamples = forested_rs, 
+    resamples = forested_rs,
     grid = 25,
     verbose = TRUE
   )
 
 # ------------------------------------------------------------------------------
 
-mlp_ranks <- 
-  mlp_grid_res |> 
+mlp_ranks <-
+  mlp_grid_res |>
   rank_results(rank_metric = "brier_class")
 
 # ------------------------------------------------------------------------------
 
-mlp_best_mtr <- 
-  mlp_grid_res |> 
-  mutate(.metrics = map(result, ~ collect_metrics(.x))) |> 
-  select(wflow_id, .metrics) |> 
+mlp_best_mtr <-
+  mlp_grid_res |>
+  mutate(.metrics = map(result, ~ collect_metrics(.x))) |>
+  select(wflow_id, .metrics) |>
   unnest(.metrics)
 
-mlp_collect <- 
-  mlp_grid_res |> 
+mlp_collect <-
+  mlp_grid_res |>
   mutate(
-    epochs = 
-      map(
-        result, 
-        ~ collect_extracts(.x))
-  ) |> 
-  select(wflow_id, epochs) |> 
-  unnest(epochs) |> 
-  filter(map_lgl(.extracts, ~ inherits(.x, "data.frame"))) |> 
+    epochs = map(
+      result,
+      ~ collect_extracts(.x)
+    )
+  ) |>
+  select(wflow_id, epochs) |>
+  unnest(epochs) |>
+  filter(map_lgl(.extracts, ~ inherits(.x, "data.frame"))) |>
   unnest(.extracts)
 
 epoch_actual <- mlp_collect
 
-mlp_collect <- 
-  mlp_collect |> 
+mlp_collect <-
+  mlp_collect |>
   summarize(
     epoch_min = min(epoch_actual),
     epoch_max = max(epoch_actual),
@@ -191,31 +191,31 @@ mlp_collect <-
     .by = c(.config, wflow_id)
   )
 
-brier_and_params <- 
-  full_join(mlp_ranks, mlp_collect, by = join_by(wflow_id, .config)) |> 
-  filter(.metric == "brier_class") |> 
+brier_and_params <-
+  full_join(mlp_ranks, mlp_collect, by = join_by(wflow_id, .config)) |>
+  filter(.metric == "brier_class") |>
   # some models failed
   filter(!is.na(num_param))
 
 # ------------------------------------------------------------------------------
 
-best_id <- 
-  mlp_ranks |> 
-  filter(.metric == "brier_class") |> 
-  slice_min(mean, n = 5) |> 
+best_id <-
+  mlp_ranks |>
+  filter(.metric == "brier_class") |>
+  slice_min(mean, n = 5) |>
   inner_join(
     brier_and_params |> select(wflow_id, .config, num_param),
     by = join_by(wflow_id, .config)
-  ) |> 
-  slice_min(num_param, n = 1) |> 
+  ) |>
+  slice_min(num_param, n = 1) |>
   pluck("wflow_id")
 
 print(best_id)
 
 # ------------------------------------------------------------------------------
 
-mlp_best_res <- 
-  mlp_grid_res |> 
+mlp_best_res <-
+  mlp_grid_res |>
   extract_workflow_set_result(id = best_id)
 
 mlp_best_config <- select_best(mlp_best_res, metric = "brier_class")
@@ -223,8 +223,13 @@ mlp_best_config <- select_best(mlp_best_res, metric = "brier_class")
 # ------------------------------------------------------------------------------
 
 save(
-  mlp_ranks, best_id, mlp_best_res, mlp_best_config, brier_and_params, 
-  mlp_best_mtr, epoch_actual,
+  mlp_ranks,
+  best_id,
+  mlp_best_res,
+  mlp_best_config,
+  brier_and_params,
+  mlp_best_mtr,
+  epoch_actual,
   file = "RData/forested_mlp.Rdata"
 )
 
